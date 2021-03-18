262dbdcf203316c9c1a5eb0941b2ce2d77396234
igorleoo1 <l.igor@acad.ifma.edu.br>
2021-03-18 13:26:40 -0300

    Atualiz. com formulário

--- resources/views/soma.blade.php
+++ resources/views/soma.blade.php
@@ -5,8 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+    <style>
+    body {
+        font-family: 'Roboto Slab', serif;
+    }
+    </style>
 </head>
 <body>
-    <h1>Calculadora</h1>
+    <div class="container">
+
+    <!-- Inicio do cabeçalho -->
+    <div class="row mt-2">
+    Cabeçalho
+    </div>
+    <!-- Final do Cab -->
+
+    <!-- Inicio do corpo -->
+    <div class="row mt-2">
+    
+        <div class="row">
+        <h1>Calculadora</h1>
+        </div>
+
+        <div class="row">
+        <form>
+  <div class="mb-3">
+    <label for="primeiroValor" class="form-label">Primeiro valor</label>
+    <input type="number" class="form-control" id="primeiroValor">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <div class="mb-3">
+    <label for="segundoValor" class="form-label">Segundo valor</label>
+    <input type="number" class="form-control" id="segundoValor">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <button type="submit" class="btn btn-primary">Somar</button>
+</form>
+        </div>
+    </div>
+    <!-- Final do Corp -->
+
+    <!-- Inicio do Rodapé -->
+    <div class="row mt-2">
+    Rodapé
+    </div>
+    <!-- Final do Rod -->
+    </div>
 </body>
 </html>
